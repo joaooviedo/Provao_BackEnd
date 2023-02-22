5e629af8b1329a20486c5636492fa6f29b34e2ed
joaooviedo <joaovitorgiroldi@gmail.com>
2023-02-22 20:17:18 -0300

    Final Commit

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
-
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
-  cors: true
+  app.set('trust proxy', 1);
 
   app.useGlobalPipes(new ValidationPipe());
+  
 
   const config = new DocumentBuilder()
     .setTitle('API')
     .setDescription('The super API description')
     .setVersion('1.0')
-    .addTag("user")
-    .addTag("auth")
+    .addTag('user')
+    .addTag('auth')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT || 3000)
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
